e85b09283079971516528c20aea93cd3e147da1c
AkifZaidi <116806308+AkifZaidi@users.noreply.github.com>
2022-10-31 02:35:35 +0500

    practice
    practice

---
+++ app.js
@@ -0,0 +1,97 @@
+// chapter 1 practice
+// ALERT
+alert("HELLO JAVASCRIPT");
+
+
+
+// chapter 2 practice
+// VARIABLES FOR STRINGS
+var myname = "akif"
+var mylastname = "zaidi"
+
+
+
+
+// chapter 3: practice
+//VARIABLES FOR NUMBERS
+var abc = 500
+var cbd = 1000
+var alphabits = abc + cbd
+alert(alphabits)
+
+
+ 
+// chapter 4: practice
+//VARIABLE NAMES LEGAL AND IllEGAL
+var first name = "akif"
+//  illegal variable
+
+var firstname = "akif"
+//  legal variable
+
+var $kill = "akif"
+//  legal variable
+
+var def = "akif"
+//  legal variable
+
+var 123 = "123"
+//  illegal variable
+
+
+var a345 = "543"
+//  legal variable
+
+var @2175 = "2175"
+//  illegal variable
+
+
+// chapter 5 ptactice
+// MATH EXPRESSIoNS FAMILIAR OPERATORS
+// it is addition variable
+var additionNumber = 12 + 24;
+alert(additionNumber)
+// it is supraction variable
+var supractionNumber = 12 - 24;
+alert(supractionNumber)
+// it is multiplication variable
+var multiplyNumber = 12 * 24;
+alert(multiplyNumber)
+// it is division  variable
+var dividehNumber = 12 / 24;
+alert(dividehNumber)
+
+// chapter 6: practice
+// MATH EXPRESSIONS UNFAMILIAR OPERATORS
+var num = 1;
+var newNum = num++;
+
+
+var num = 1;
+var newNum = num--;
+
+
+// chapter7 : practice
+// MATH EXPRESSIONS ELIMINATING AMBIGUITY
+var amount = 5 + (7 * 9);
+// use (BODMAS) rule
+
+
+//  var message = "Thanks, ";
+//  var banger = "!";
+//  alert(message + userName + banger);
+
+// chapter 8: practice
+// CONCATENATING TEXT STRTNGS
+var first_name = "syed akif"
+var lastname = "hussain zaidi"
+alert(first_name + "" + lastname )
+// this is concatenation
+
+
+
+
+
+
+
+
